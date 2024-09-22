77398612f3aa3db3a111e34bc49a32bff4d3297b
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-09-22 14:54:29 +0300

    add contracts

---
+++ SaccoOps/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace SaccoOps.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder.AllowAnyHeader().AllowAnyOrigin().AllowAnyHeader());
+            });
+        }
+        public static void ConfigureIISOptions(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options => { });
+        }
+    }
+}
