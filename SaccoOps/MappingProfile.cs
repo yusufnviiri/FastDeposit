7fda55f8339593b3339e496b43f17742983af62b
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-09-25 14:39:21 +0300

    add id

--- SaccoOps/MappingProfile.cs
+++ SaccoOps/MappingProfile.cs
@@ -9,8 +9,8 @@ namespace SaccoOps
         public MappingProfile()
         {
             //CreateMap<Deposit,ShowSaccoTransactionDto>().ForCtorParam("ModelId", opt=>opt.MapFrom(p=>p.DepositId));
-            //CreateMap<Deposit, ShowSaccoTransactionDto>().ForMember(k=>k.ModelId, opt => opt.MapFrom(p => p.DepositId));
-            CreateMap<Withdraw, ShowSaccoTransactionDto>();
+            CreateMap<Deposit, ShowSaccoTransactionDto>().ForMember(k=>k.Id, opt => opt.MapFrom(p => p.DepositId));
+            //CreateMap<Withdraw, ShowSaccoTransactionDto>();
             CreateMap<Deposit, ShowSaccoTransactionDto>();
            // CreateMap< ShowSaccoTransactionDto, Deposit>();
             CreateMap<UserRegistrationDto, User>();
--- Shared/DataTransferObjects/ShowSaccoTransactionDto.cs
+++ Shared/DataTransferObjects/ShowSaccoTransactionDto.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Shared.DataTransferObjects
 {
-    public record ShowSaccoTransactionDto(int Id, decimal Amount, decimal Balance,string TransactionDate);
- 
+    //public record ShowSaccoTransactionDto(int DepopsitId, decimal Amount, decimal Balance,string TransactionDate,string UserId);
+    //public record ShowSaccoTransactionDto( decimal Amount, decimal Balance, string TransactionDate);
+    public record ShowSaccoTransactionDto(int Id, decimal Amount, decimal Balance, string TransactionDate);
+
 }
